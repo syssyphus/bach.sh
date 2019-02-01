b2e68f870b79926594242160e6b78511535779c6
Olga Ustuzhanina <me@laserbat.pw>
2019-02-01 14:30:42 +0700

    Replaced synth with square wave, removed dependency on anything besides POSIX shell

--- bach.sh
+++ bach.sh
@@ -20,10 +20,24 @@ note() {
     DURATION=${2:-4}
 
     if [ "$FREQ" != "P" ]; then
-        LEN=$((SAMPLE_RATE / FREQ - 1))
+        LEN=$((SAMPLE_RATE / FREQ))
+        PW=$((LEN / 2))
 
-        WAVEFORM="$(tail $0 | base64 -d | head -c $LEN)"
-        yes "$WAVEFORM" | head -c $((SAMPLE_RATE / DURATION))
+        MIN=$(printf '\x01')
+        MAX=$(printf '\xFF')
+        WAVEFORM=""
+
+        for i in $(seq $PW); do
+            WAVEFORM="$MIN$WAVEFORM"
+        done
+
+        for i in $(seq $PW); do
+            WAVEFORM="$MAX$WAVEFORM"
+        done
+
+        for i in $(seq $((SAMPLE_RATE / (DURATION * LEN)))); do
+            printf "$WAVEFORM"
+        done
     fi
 }
 
@@ -52,16 +66,10 @@ arp ${B[2]} ${C[2]} ${E[2]} ${G[2]} ${C[3]}
 
 arp ${A[2]} ${C[2]} ${E[2]} ${G[2]} ${C[3]}
 arp ${D[1]} ${A[2]} ${D[2]} ${F_sharp[2]} ${C[3]}
-arp ${G[1]} ${A[2]} ${D[2]} ${G[2]} ${B[3]}
+arp ${G[1]} ${B[2]} ${D[2]} ${G[2]} ${B[3]}
+arp ${G[1]} ${A_sharp[2]} ${E[2]} ${G[2]} ${C_sharp[3]}
 
-exit
-cXFxb2xrZ2VjZGZmZ2lsbW5tbm1samloaGZjYWBfXFxbWltbXF1hYmRoamxvcnN2d3h4eHp8fX5/gYSHioyOjY6Ojo2LioeEgH18
-fHx9f4GDhouPlJaanqKnqaysra2vr62qpqKfnJqZmZmXlZORkZGRkZCPjo6OjY2PkJGRkJCOjY2Oj5CQkZKSkpGQjYqFgX16eHZy
-bWppaWhmZGJhYWJkZGVkZGVlZmZmZGRiYGFfXl5eXl1dXVtcXV5eX2FiYmVoa25ydnl6fYCDhomNkJKVl5ugpKesr7GytLOysbKw
-r62tq6ikoJuWko2JhIB+fnx8fX5+foGDg4SFhISCgHx5d3V0dHV2eHp7fYCEiIyOkJKTlpeYmJeVlZSVk5KSkpKQjYqIhIF/fHl3
-c25oYl1ZV1RSUVBRUlNWWVxeYGBgYmJkZWVlZmhqbG1tbGxtb3FwcHJ0dXd3dnV1dHNycHFydHZ2dnV1c3N0dXV0cm9ua2pqa21v
-c3R0dHR0dXd5en2Ag4WGiIqKi42Oj5CRkZKSlJWWlpaVlpaZmpqcnqGkp6msr7Kztre2tbOztLS2ubu9vLu6uLW0s7Kwramop6ip
-qKqrra+ytba2tra4uLi4uLi5urq6vLy8vb6+wMDAv768vLq3tbOxr62sq6moqKmop6alop6bmJeVlJWVk5CPjYmHhYSCgH59e3p6
-fH5/fnx7end2c3FvbWpoZmNhX1xcXF5fXl5eX2BhYWJiYWFhY2NjY2NlZ2ttbm5ubGpqa2pra2pnZ2hqbG5xdHZ3d3h6fH2Ag4WH
-i46Pj5CQjoyLi4yNjYyLiYaBfn17eXh1cm5qZmBcWFRSUU9OS0lISEdFREJCQkJDRUlNUldaW11eX2FiZWdpbG1vcHFwbWpmY19d
-WlZRTk1OT1JUWFxgYWJiYmFhYmRlZmZmZmVkZWVkY2NhYF5cWlpbXF1fYWRmaGpqa2ppaWhoZ2RiYWJhYWFhY2VmaGloampsbnJ2
+arp ${F[1]} ${A[2]} ${D[2]} ${A[3]} ${D[3]}
+arp ${F[1]} ${G_sharp[1]} ${D[2]} ${F[2]} ${B[3]}
+arp ${E[1]} ${G[1]} ${C[2]} ${G[2]} ${C[3]}
+arp ${E[1]} ${F[1]} ${A[2]} ${C[2]} ${F[2]}
